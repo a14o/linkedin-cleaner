13c9fd97797ba029d9e6e6cfa7b5b9e9509bfdd8
hello-isa <isabeljoyadriatico@gmail.com>
2026-03-10 10:22:34 +0800

    include navigation

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 
 const { startBrowser } = require('./auth/login');
+const { goToActivity } = require("./actions/navigate");
 const { runDeletionLoop } = require('./actions/deleteLoop');
 
 (async () => {
   const { browser, page } = await startBrowser();
+
+  await goToActivity(page);
 
   await runDeletionLoop(page);
 
